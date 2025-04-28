d9abdbef52f07167d563f68b5ef16038130fceb2
Maddy <124234301+Nitrolord@users.noreply.github.com>
2025-04-28 11:19:13 -0400

    Update Spinner.lua

--- Killer Blocks/Spinner.lua
+++ Killer Blocks/Spinner.lua
@@ -1,3 +1,5 @@
+-- Put inside the long red neon part over a large hollow circle
+
 -- Spins the part it is put in
 
 while true do script.Parent.CFrame = script.Parent.CFrame * CFrame.fromEulerAnglesXYZ(0,0.1,0.0,0) wait() end
