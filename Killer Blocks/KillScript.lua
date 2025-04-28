b1d625765934986bf4f72bff8a476b3432259915
Maddy <124234301+Nitrolord@users.noreply.github.com>
2025-04-28 11:15:57 -0400

    Update KillScript.lua

--- Killer Blocks/KillScript.lua
+++ Killer Blocks/KillScript.lua
@@ -1,4 +1,5 @@
 -- Kills player who touches the part it is put in
+
 script.Parent.Touched:connect(function(hit)
 	if hit and hit.Parent and hit.Parent:FindFirstChild("Humanoid") then
 		hit.Parent.Humanoid.Health = 0
